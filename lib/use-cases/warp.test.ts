65e87d4ad721ec73c564422cc15f832d19ed31d2
sins <sinsmailza@gmail.com>
2026-02-10 16:34:52 +0000

    try to get tests working

--- lib/use-cases/warp.test.ts
+++ lib/use-cases/warp.test.ts
@@ -1,7 +1,11 @@
-import { describe, it, expect, afterEach } from "vitest";
+import { describe, it, expect, beforeAll, afterEach } from "vitest";
 import nock from "nock";
-import { getAuthToken } from "./warp.ts";
+import { getAuthToken, getProjects } from "./warp.ts";
 import { emailSchema } from "../schemas/warp.ts";
+
+beforeAll(() => {
+  nock.disableNetConnect();
+});
 
 afterEach(() => {
   nock.restore();
@@ -11,7 +15,7 @@ describe("getAuthToken", async () => {
   it("should return a token ", async () => {
     const testEmail = emailSchema.parse("some@email.com");
     const testPassword = "someTestPassword";
-    const correctResponse = { token: "someToken" }
+    const correctResponse = { token: "someToken" };
 
     nock("https://office.warpdevelopment.com")
       .post("/api/account/authorise", {
@@ -21,8 +25,14 @@ describe("getAuthToken", async () => {
       .reply(200, correctResponse);
 
     const result = await getAuthToken(testEmail, testPassword);
-    if (result.isErr()) return console.log(result.error);
-    expect(result.value).toBe(correctResponse.token);
+
+    expect(result.isOk()).toBe(true);
+
+    if (result.isErr()) {
+      throw new Error(result.error);
+    }
+
+    expect(result.value).toEqual(correctResponse.token);
   });
   it("should return an error result if request failes", async () => {
     const testEmail = emailSchema.parse("some@email.com");
@@ -33,7 +43,7 @@ describe("getAuthToken", async () => {
         Email: testEmail,
         Password: testPassword,
       })
-      .replyWithError("Network failure")
+      .replyWithError("Network failure");
 
     const result = await getAuthToken(testEmail, testPassword);
     expect(result.isErr()).toBe(true);
@@ -47,9 +57,54 @@ describe("getAuthToken", async () => {
         Email: testEmail,
         Password: testPassword,
       })
-      .reply(200, { someNonsense: "" })
+      .reply(200, { someNonsense: "" });
 
     const result = await getAuthToken(testEmail, testPassword);
     expect(result.isErr()).toBe(true);
+  });
+});
+
+describe("getProjects", async () => {
+  it("should return projects ", async () => {
+    const testToken = "someBearerToken";
+    const correctResponse = [
+      {
+        TaskId: 2,
+        Name: "Hosting Website",
+        IsActive: true,
+        Created_On: "2007-01-10T14:49:26.093",
+        Updated_On: "2007-01-10T14:49:26.093",
+        Client: {
+          GroupId: 2,
+          Name: "Warp Development",
+          Currency: "South African Rand",
+        },
+      },
+      {
+        TaskId: 3,
+        Name: "Software Website",
+        IsActive: true,
+        Created_On: "2007-01-10T14:49:31.03",
+        Updated_On: "2020-06-01T13:55:18.21",
+        Client: {
+          GroupId: 2,
+          Name: "Warp Development",
+          Currency: "South African Rand",
+        },
+      },
+    ];
+
+    nock("https://office.warpdevelopment.com")
+      .get("/api/Project")
+      .matchHeader("authorization", `Bearer ${testToken}`)
+      .reply(200, correctResponse);
+
+    const result = await getProjects(testToken);
+
+    if (result.isErr()) {
+      throw new Error(result.error);
+    }
+    expect(result.isOk()).toBe(true);
+    expect(result.value).toEqual(correctResponse);
   });
 });
--- lib/use-cases/warp.ts
+++ lib/use-cases/warp.ts
@@ -1,8 +1,8 @@
 import { ENDPOINTS as warpEndpoints } from "../constants/warp.ts";
-import type { WarpEmail } from "../types/warp.ts";
+import type { WarpEmail, Project } from "../types/warp.ts";
 import { Result, ResultAsync, err, ok } from "neverthrow";
 import { got } from "got";
-import { tokenResponseSchema } from "../schemas/warp.ts";
+import { tokenResponseSchema, projectSchema } from "../schemas/warp.ts";
 
 export async function getAuthToken(
   email: WarpEmail,
@@ -31,7 +31,39 @@ export async function getAuthToken(
     (e) => new Error(`Parse failed, err: ${String(e)}`),
   )();
 
-  if (parseResult.isErr()) return err(`Token parsing failed, err: ${parseResult.error}`);
+  if (parseResult.isErr())
+    return err(`Token parsing failed, err: ${parseResult.error}`);
 
   return ok(parseResult.value.token);
+}
+
+export async function getProjects(
+  token: string,
+): Promise<Result<Project[], string>> {
+  const result = await ResultAsync.fromPromise(
+    got
+      (warpEndpoints.getProjects.url, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+      })
+      .json(),
+    (e) =>
+      new Error(
+        `Request to ${warpEndpoints.getProjects.url} failed: ${String(e)}`,
+      ),
+  );
+
+  if (result.isErr())
+    return err(`Error getting projects, err: ${result.error}`);
+
+  const parseResult = Result.fromThrowable(
+    () => projectSchema.array().parse(result.value),
+    (e) => new Error(`Parse failed, err: ${String(e)}`),
+  )();
+
+  if (parseResult.isErr())
+    return err(`Project parsing failed, err: ${parseResult.error}`);
+
+  return ok(parseResult.value);
 }
