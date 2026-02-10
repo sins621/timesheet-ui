3681d0318a4b9a903a510c3e1535ab5601552c39
sins <sinsmailza@gmail.com>
2026-02-10 16:34:47 +0000

    add projects get

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
 import { env } from "./env.ts";
-import { getAuthToken } from "./lib/use-cases/warp.ts";
+import { getAuthToken, getProjects } from "./lib/use-cases/warp.ts";
 import { emailSchema } from "./lib/schemas/warp.ts";
 
 async function main() {
-  const result = await getAuthToken(
+  const tokenResult = await getAuthToken(
     emailSchema.parse(env.TEST_WARP_EMAIL),
     env.TEST_WARP_PASSWORD,
   );
-  console.log(result);
+  if (tokenResult.isErr()) throw new Error("Error getting token")
+  const projectsResult = await getProjects(tokenResult.value);
+  console.log(projectsResult);
 }
 
 (async () => main())();
