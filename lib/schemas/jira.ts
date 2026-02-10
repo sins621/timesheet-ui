c06a05d7b4a9cd5c13661d3a3237d247ed26d767
sins <sinsmailza@gmail.com>
2026-02-10 22:18:19 +0200

    add jira schemas

---
+++ lib/schemas/jira.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+export const avatarUrlsSchema = z.
+  object({
+    "48x48": z.url(),
+    "24x24": z.url(),
+    "16x16": z.url(),
+    "32x32": z.url(),
+  })
+  .brand<"jiraAvatarsUrl">();
+
+export const projectCategorySchema = z.
+  object({
+    self: z.url(),
+    id: z.number(),
+    description: z.string()
+  })
+  .brand<"jiraProjectCategory">();
+
+export const projectSchema = z.
+  object({
+    expand: z.string(),
+    self: z.url(),
+    id: z.number(),
+    key: z.string(),
+    name: z.string(),
+    avatarUrls: avatarUrlsSchema,
+    projectCategory: projectCategorySchema.optional(),
+    projectTypeKey: z.string(),
+    simplified: z.boolean(),
+    style: z.string(),
+    isPrivate: z.boolean(),
+    properties: z.object().optional()
+  })
+  .brand<"jiraProjectSchema">();
