afde34d3ccf8857ae71684b42f1e49a29bbd952e
sins <sinsmailza@gmail.com>
2026-02-14 13:42:25 +0000

    update jira url constant

--- lib/constants/jira.ts
+++ lib/constants/jira.ts
@@ -1,7 +1,8 @@
-export const API_URL = "https://warpdevelopment.atlassian.net/rest/api/3/";
+import { urlSchema } from "../schemas/common.ts";
 
+export const API_URL = "https://warpdevelopment.atlassian.net/rest/api/3/";
 export const ENDPOINTS = {
-  me: {
+  projectSearch: {
     method: "GET",
     url: urlSchema.parse(API_URL + "project"),
   },
